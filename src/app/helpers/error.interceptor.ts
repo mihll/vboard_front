946fb26c0e71452869ef66bcbc1ac5f31b83eb2d
Michal Kierzkowski <kierzkowski89@gmail.com>
2021-01-29 23:04:59 +0100

    Fix error interceptor now redirects after error response

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -4,17 +4,23 @@ import { Observable, throwError } from 'rxjs';
 import { AuthenticationService } from '../services/authentication/authentication.service';
 import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../services/snackbar/snackbar.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router,
+              private snackBarService: SnackbarService
+  ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(catchError(err => {
       if ([0, 401, 403].includes(err.status) && this.authenticationService.userValue) {
         // auto logout if 401 or 403 response returned from api
         this.authenticationService.logout();
+        this.router.navigate(['/']).then(() => this.snackBarService.openErrorSnackbar('Zostałeś wylogowany!'));
       }
 
       if ([401].includes(err.status) && request.url.startsWith(environment.apiUrl.concat('/refresh'))) {
